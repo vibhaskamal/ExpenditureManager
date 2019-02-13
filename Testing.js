a756d5e6b9abee20528cab676cd25b852f7ddae1
Vibhas <vkam0002@student.monash.edu>
2019-02-14 10:47:50 +1100

    Can add amounts and dispay it in a different input box now

--- Testing.js
+++ Testing.js
@@ -1,13 +1,11 @@
-document.getElementById("testing").innerHTML = "Hello JS";
-
-console.log("Testing the functioning of console.log()");
-var x = myFunction();
+var 
 
 function myFunction() {
     if (document.getElementById("foodInfo")){
         var foodInput = document.getElementById("foodInfo").value;
-        console.log(foodInput);
-        console.log("Hello");
+        var foodValue = parseInt(foodInput);
+        foodValue += 100;
+        document.getElementById("output").value = foodValue;
     }
     
   }
