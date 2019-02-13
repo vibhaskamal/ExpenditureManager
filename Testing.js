23363a1fcb2c4aafdbfe70809b9aae75b27717ad
Vibhas <vkam0002@student.monash.edu>
2019-02-14 02:03:22 +1100

    Button created, along with text field

--- Testing.js
+++ Testing.js
@@ -1,1 +1,13 @@
 document.getElementById("testing").innerHTML = "Hello JS";
+
+console.log("Testing the functioning of console.log()");
+var x = myFunction();
+
+function myFunction() {
+    if (document.getElementById("foodInfo")){
+        var foodInput = document.getElementById("foodInfo").value;
+        console.log(foodInput);
+        console.log("Hello");
+    }
+    
+  }
