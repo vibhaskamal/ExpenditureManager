cd6c8a7bbfe593ab44855ee8d3a0f2e08f8d7eda
Vibhas <vkam0002@student.monash.edu>
2019-02-14 16:54:20 +1100

    Added the Materialize framework

--- Testing.js
+++ Testing.js
@@ -1,11 +1,18 @@
-var 
+function sum() {
 
-function myFunction() {
-    if (document.getElementById("foodInfo")){
-        var foodInput = document.getElementById("foodInfo").value;
-        var foodValue = parseInt(foodInput);
-        foodValue += 100;
-        document.getElementById("output").value = foodValue;
-    }
+    var foodInput = document.getElementById("food").value;
+    var groceriesInput = document.getElementById("groceries").value;
+    var otherInput = document.getElementById("other").value;
+
+    var categories = [foodInput, groceriesInput, otherInput];
+    var sum = 0;
+
+    for (i = 0; i < categories.length; i++) { 
+        var intParsedInput = parseInt(categories[i]);
+        sum += intParsedInput;
+      }
+
+    document.getElementById("output").value = sum;
+
     
   }
